d71e3fa73facab1459ecb62b8e270d37193dd48a
Johan <skogsjohans@gmail.com>
2019-08-28 17:11:55 +0200

    Added some code for parsing html page. Also added full purchace order manually

--- coffeestats.py
+++ coffeestats.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+#my_url ='https://en.wikipedia.org/wiki/Jacopo_Riccati'
+my_url = 'https://kortladdning3.chalmerskonferens.se/CardLoad_Order.aspx'
+# Creating a connection to the page
+uClient = uReq(my_url)
+# Reading the page
+page_html = uClient.read()
+uClient.close()
+# Parse the html code
+page_soup = soup(page_html, "html.parser")
+print(page_soup.h1)
